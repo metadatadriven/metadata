a1fe6cec85d00b549e4f105a1bb5d5fb34dc4af3
Stuart Malcolm <stuart@metadatadriven.com>
2022-08-27 07:44:35 +0000

    set root path

--- src/start.R
+++ src/start.R
@@ -1,7 +1,7 @@
 library(shidashi)
 
 # Render this project
-shidashi::template_settings$set(root_path = 'C:\\Users\\StuartMalcolm\\work\\metadata\\src')
+shidashi::template_settings$set(root_path = './src')
 
 # Render project
 shidashi::render(host = '127.0.0.1', port = 8310L, as_job = FALSE)
