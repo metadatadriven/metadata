5eb5d32f9962ddf4efe96ab692ad0315288b1598
Stuart Malcolm <stuart@metadatadriven.com>
2022-08-27 08:25:55 +0000

    shell script to start app

--- app.sh
+++ app.sh
@@ -1,6 +1,9 @@
-# install dependencies..
-# logger
-# install.packages(c("ggExtra", "rmarkdown"))
-# install.packages("shidashi")
+#!/bin/bash
 
-# THEN START SHINY APP IN ./src
+# install dependencies..
+R -e 'install.packages("logger")'
+R -e 'install.packages(c("ggExtra", "rmarkdown"))'
+R -e 'install.packages("shidashi")'
+
+# START SHINY APP
+R -e 'shiny::runApp("./src", port=8888, host="0.0.0.0")'
