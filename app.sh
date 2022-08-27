705e3e200f3a5df94d43610826b2f83d0c9ebdf5
Stuart Malcolm <stuart@metadatadriven.com>
2022-08-27 08:39:02 +0000

    use local library (giitpod)

--- app.sh
+++ app.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# create local user library path (if not already present)
+R -e 'dir.create(path = Sys.getenv("R_LIBS_USER"), showWarnings = FALSE, recursive = TRUE)'
+
 # install dependencies..
-R -e 'install.packages("logger")'
-R -e 'install.packages(c("ggExtra", "rmarkdown"))'
-R -e 'install.packages("shidashi")'
+R -e 'install.packages("shiny", lib = Sys.getenv("R_LIBS_USER"))'
+R -e 'install.packages("logger", lib = Sys.getenv("R_LIBS_USER"))'
+R -e 'install.packages(c("ggExtra", "rmarkdown"), lib = Sys.getenv("R_LIBS_USER"))'
+R -e 'install.packages("shidashi", lib = Sys.getenv("R_LIBS_USER"))'
 
 # START SHINY APP
 R -e 'shiny::runApp("./src", port=8888, host="0.0.0.0")'
