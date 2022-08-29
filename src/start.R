91e3c3920f535bf492cff0c1924929ea30f2be13
Stuart Malcolm <stuart.malcolm@veramed.co.uk>
2022-08-29 04:15:53 -0700

    update to use runpath env var
    Pushed from Domino: https://se-sandbox.domino-eval.com/workspace/stuartm/metadata?executionId=630c9d68c92bbb395372f6af

--- src/start.R
+++ src/start.R
@@ -1,7 +1,7 @@
 library(shidashi)
 
 # Render this project
-shidashi::template_settings$set(root_path = './src')
+shidashi::template_settings$set(root_path = file.path(Sys.getenv("runpath"),"src"))
 
 # Render project
 shidashi::render(host = '127.0.0.1', port = 8310L)
